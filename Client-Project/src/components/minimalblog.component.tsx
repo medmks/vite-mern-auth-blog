a14777e368c7ba09b4820c9431b0de330e15c3cb
Mohamed <looklike882@gmail.com>
2024-06-20 14:44:12 +0100

    Add typed Minimal Blog

--- Client-Project/src/components/minimalblog.component.tsx
+++ Client-Project/src/components/minimalblog.component.tsx
@@ -1,24 +1,40 @@
 import { Link } from "react-router-dom";
 import { Getthedate } from "../common/date";
+import type { TrendyBlog } from "../pages/Home.page";
+/**
+ * Renders a minimal blog component.
+ *
+ * @param {Object} props - The component props.
+ * @param {TrendsBlogDocument} props.blog - The blog document.
+ * @param {number} props.index - The index of the blog.
+ * @return {JSX.Element} The rendered minimal blog component.
+ */
 
-const Minimalblog = ({ trends, index }: { trends, index: number }) => {
+const Minimalblog: React.FC<{ blog: TrendyBlog; index: number }> = (props: {
+  blog: TrendsBlogDocument;
+  index: number;
+}): JSX.Element => {
+  const { blog, index } = props;
+  const authorName = blog.author.name as string;
+  const blogTitle = blog.title;
+  const blogCreatedDate = Getthedate(blog.createdAt);
 
   return (
     <Link to={"/"} className="flex gap-5 mb-5 ">
-      <h1 className="blog-index">{index < 10 ? "0" + (index + 1) : index}</h1>
+      <h1 className="blog-index">{index < 10 ? `0${index + 1}` : index + 1}</h1>
       <div>
-        <div className=" flex flex-row gap-2 items-center mb-7 ">
+        <div className="flex flex-row gap-2 items-center mb-7">
           <img
             src="https://media.gettyimages.com/id/181048438/fr/photo/maroc-dans-une-mosqu%C3%A9e.jpg?s=612x612&w=gi&k=20&c=Ahq7S3qxdrl2CHhUwOnHUpA5ICz8yIc4N_oZ5IhM-Dw="
             className="w-6 h-6 rounded-full"
-            alt=""
+            alt={authorName}
           />
-          <p className=" line-clamp-1">
-            {trends.author.name} @{trends.author.name}
+          <p className="line-clamp-1">
+            {authorName} @{authorName}
           </p>
-          <p className="min-w-fit"> {Getthedate(trends.createdAt)}</p>
+          <p className="min-w-fit">{blogCreatedDate}</p>
         </div>
-        <h1 className=" blog-title">{trends.title}</h1>
+        <h1 className="blog-title">{blogTitle}</h1>
       </div>
     </Link>
   );
